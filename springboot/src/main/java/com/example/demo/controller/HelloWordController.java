99fc95f0801ba3c9d9010e63c94b51a6cc0b8e15
Administrator <Administrator@14CISAEYS35MX2L>
2019-11-09 08:40:38 +0800

    黄醒提交

--- springboot/src/main/java/com/example/demo/controller/HelloWordController.java
+++ springboot/src/main/java/com/example/demo/controller/HelloWordController.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-//新秀注释
+
 @Controller
 public class HelloWordController {
     @RequestMapping("/hello")
