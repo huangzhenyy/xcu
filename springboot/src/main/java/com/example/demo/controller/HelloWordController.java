6e41d92d00cecb9aa06773c2092b054d505d9bb7
Administrator <Administrator@14CISAEYS35MX2L>
2019-11-09 08:42:49 +0800

    再次提交

--- springboot/src/main/java/com/example/demo/controller/HelloWordController.java
+++ springboot/src/main/java/com/example/demo/controller/HelloWordController.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+//再次修改
 @Controller
 public class HelloWordController {
     @RequestMapping("/hello")
