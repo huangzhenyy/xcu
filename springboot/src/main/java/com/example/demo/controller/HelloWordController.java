7e1abe809cba78598b29820ed6dd1288ff26be34
Huang <Huang@DELL>
2019-11-09 08:35:54 +0800

    修改内容：后台控制

--- springboot/src/main/java/com/example/demo/controller/HelloWordController.java
+++ springboot/src/main/java/com/example/demo/controller/HelloWordController.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+//后台控制
 @Controller
 public class HelloWordController {
     @RequestMapping("/hello")
